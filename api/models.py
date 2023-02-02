6db8c5d4ec102243e971b5f489ba064ccd5270e6
Rajat <rajat.july15@gmail.com>
2023-02-02 17:20:26 +0530

    added default values

--- api/models.py
+++ api/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 class Record(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255, null=True)
-    amount = models.FloatField(null=True)
+    amount = models.FloatField(null=True,blank=True,default=0.0)
     sku = models.CharField(max_length=100)
     qty = models.IntegerField(default=1)
     is_replacement = models.BooleanField(default=False)
-    remarks = models.TextField(default="")
+    remarks = models.TextField(default="",null=True,blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
