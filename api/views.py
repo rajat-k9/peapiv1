5f89807b15755d237c2586c6e4e3605a3d2329ba
Rajat <rajat.july15@gmail.com>
2023-02-02 13:48:20 +0530

    added permission class

--- api/views.py
+++ api/views.py
@@ -3,12 +3,14 @@ from api.models import Customer,Record
 from rest_framework import  viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer,CustomerSerializer,RecordSerializer
+from rest_framework import permissions
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
