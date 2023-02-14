dcbcb2d00ebfe2a068b87b7e55a148f1f633bf9b
Rajat <rajat.july15@gmail.com>
2023-02-14 11:42:33 +0530

    added whatsapp alert

---
+++ api/scripts/whatsapp.py
@@ -0,0 +1,54 @@
+from whatsapp_api_client_python import API as API
+from html2image import Html2Image
+from api.models import Stock
+from datetime import date
+
+
+
+ID_INSTANCE = '1101789716'
+API_TOKEN_INSTANCE = '2ade3716cb384116af5af77ba4b4fa55d186b7f55f294a9793'
+GROUP_ID = '120363031328070036@g.us'
+
+greenAPI = API.GreenApi(ID_INSTANCE, API_TOKEN_INSTANCE)
+
+class Whatsapp():
+    def send_alert(self):
+        print("schduler created")
+        html = "<table border='1'><tbody><tr><th>Name</th><th>Home</th><th>Shop</th></tr>"
+        # Get stock data
+        stock_queryset = Stock.objects.all()
+        for obj in stock_queryset:
+            html = html + "<tr><td>"+obj.name+"</td><td>"+str(obj.home)+"</td><td>"+str(obj.shop)+"</td></tr>"
+        html = html + "</tbody></table>"
+        hti = Html2Image()
+
+        css = 'body {background: white;}'
+
+        # screenshot an HTML string (css is optional)
+        hti.screenshot(html_str=html, css_str=css, save_as='stock.png', size=(300, 500))
+        # with open("page.png","r") as file:
+        #     x = file.name
+        #     print(x)
+
+        # chatIds = [
+        #     "917503175945@c.us","919044883644@c.us"
+        # ]
+        # resultCreate = greenAPI.groups.createGroup('Stock Alerts', 
+        #     chatIds)
+
+        # if resultCreate.code == 200:
+        #     print(resultCreate.data)
+            # resultSend = greenAPI.sending.sendMessage(resultCreate.data['chatId'], 'Message text')
+        # resultSend = greenAPI.sending.sendMessage(GROUP_ID, 'Message text')
+        today = date.today()
+        resultSend = greenAPI.sending.sendFileByUpload(GROUP_ID, 
+            'stock.png','stock.png','Wire Stock on '+today.strftime("%d-%m-%Y"))
+        if resultSend.code == 200:
+            print(resultSend.data)
+        else:
+            print(resultSend.error)    
+        # else:
+        #     print(resultCreate.error)
+
+
+    
