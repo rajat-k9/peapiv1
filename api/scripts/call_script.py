8bf36c4252a871a9b4694a6f73c6e6d82b9bec04
Rajat <rajat.july15@gmail.com>
2023-02-15 09:26:40 +0530

    added cron job

--- api/scripts/call_script.py
+++ api/scripts/call_script.py
@@ -1,15 +1,26 @@
 from .whatsapp import Whatsapp
 from apscheduler.schedulers.background import BackgroundScheduler
-
+from apscheduler.triggers.cron import CronTrigger
 
 def start():
     what_obj = Whatsapp()
     scheduler = BackgroundScheduler()
+    trigger = CronTrigger(
+        year="*", month="*", day="*", hour="9", minute="27", second="0"
+    )
+    scheduler.add_job(
+        what_obj.send_alert,
+        trigger=trigger,
+        id="stockalertcron",
+    )
+    
     
     # scheduler.add_job(what_obj.send_alert, 'interval', seconds=59)
     # Runs from Monday to Friday at 6:30AM until 2022-06-30 00:00:00
-    scheduler.add_job(what_obj.send_alert, 'cron', day_of_week= 'mon-sun', 
-        hour= 6, minute= 30, id="stockalert")
-    # scheduler.print_jobs()
+    # try:
+    #     scheduler.add_job(what_obj.send_alert, 'cron', minute= 1, id="stockalert")
+    # except Exception as e:
+    #     raise Exception(e)
+    scheduler.print_jobs()
     scheduler.start()
     # scheduler.shutdown()
